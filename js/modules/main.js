dbc7c76fddc651b813ae0ae9977aa7d50623482c
Gabriel <112393502+gabriell-hb@users.noreply.github.com>
2022-12-07 17:59:51 +0000

    Defined variables for the timer

---
+++ js/modules/main.js
@@ -0,0 +1,6 @@
+const studyButton = document.querySelector('.timer__study');
+const shortBreakButton = document.querySelector('.timer__short-break');
+const longBreakButton = document.querySelector('.timer__long-break');
+const clock = document.querySelector('.timer__clock');
+const startButton = document.querySelector('.timer__start');
+const stopButton = document.querySelector('.timer__stop');
