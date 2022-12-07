bbfa3166795023e8186b18415b0d7f101cfc699c
Gabriel <112393502+gabriell-hb@users.noreply.github.com>
2022-12-07 18:03:02 +0000

    Added event listeners to the timer

--- js/modules/main.js
+++ js/modules/main.js
@@ -4,3 +4,9 @@ const longBreakButton = document.querySelector('.timer__long-break');
 const clock = document.querySelector('.timer__clock');
 const startButton = document.querySelector('.timer__start');
 const stopButton = document.querySelector('.timer__stop');
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+studyButton.addEventListener('click', resetTimer);
+shortBreakButton.addEventListener('click', handleShortBreak);
+longBreakButton.addEventListener('click', handleLongBreak);
