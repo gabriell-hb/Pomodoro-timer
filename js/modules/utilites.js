375f7c54ff9246a03bd2f568ca37385294079ce2
Gabriel <112393502+gabriell-hb@users.noreply.github.com>
2022-12-07 18:01:32 +0000

    Added function to format the timer

---
+++ js/modules/utilites.js
@@ -0,0 +1,13 @@
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+
+  return `${minutes}:${seconds}`;
+}
