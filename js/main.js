f91919151022ee2d4663687a197ecfb297118c4f
Gabriel <112393502+gabriell-hb@users.noreply.github.com>
2022-12-07 18:58:03 +0000

    Added comments

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// ahhh klarer ikke å importe fra de andre js filene ://
+
+
+/* import {studyButton, shortBreakButton, longBreakButton, clock, startButton, stopButton} from './js/modules/variables_handlers.js';
+
+import {time, isRunning, startTimer, stopTimer, resetTimer, handleShortBreak, handleLongBreak} from './js/modules/timer.js';
+
+import {formatTime} from './js/modules/utilities.js'; */
+
+
+// // developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+
